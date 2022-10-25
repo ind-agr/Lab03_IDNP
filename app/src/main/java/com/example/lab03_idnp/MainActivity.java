5484a58aab1ec4a5794d1bd84d67df78daf0107d
ind-agr <iagramontem@unsa.edu.pe>
2022-10-25 12:37:49 -0500

    Llamado a MenuActivity con intent

--- app/src/main/java/com/example/lab03_idnp/MainActivity.java
+++ app/src/main/java/com/example/lab03_idnp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.lab03_idnp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMenu = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intentMenu);
+            }
+        });
     }
 }
