9b366b22717fb6f57ccbadf72ebb554419232488
ind-agr <iagramontem@unsa.edu.pe>
2022-10-25 14:18:53 -0500

    Implementación de la interfaz de registro, clase Postulante, llamada a la clase Portulante Registro con intent

--- app/src/main/java/com/example/lab03_idnp/MenuActivity.java
+++ app/src/main/java/com/example/lab03_idnp/MenuActivity.java
@@ -2,13 +2,27 @@ package com.example.lab03_idnp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+    Button btnRegNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnRegNew = findViewById(R.id.btnNuevo);
+        btnRegNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegistro = new Intent(MenuActivity.this,PostulanteRegistroActivity.class);
+                startActivity(intentRegistro);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/lab03_idnp/Postulante.java
@@ -0,0 +1,67 @@
+package com.example.lab03_idnp;
+
+public class Postulante {
+    private int dni;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String fechaNac;
+    private String colegio;
+    private String carrera;
+
+    public int getDni(){
+        return  dni;
+    }
+
+    public  void setDni(int dni){
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getColegio() {
+        return colegio;
+    }
+
+    public void setColegio(String colegio) {
+        this.colegio = colegio;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
